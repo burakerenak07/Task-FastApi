f4629ae85cb85b88db8f74790971b0dbc99353bf
Burak Eren AK <burakeren.ak@hotmail.com>
2023-03-28 21:36:18 +0300

    task

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI, BackgroundTasks, HTTPException
-from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 from extract import *
 import os
-from fastapi import FastAPI, Form, Request
-
 
 
 SECRET = os.getenv("SECRET")
@@ -38,29 +35,3 @@ async def demo_post(inp: Msg, background_tasks: BackgroundTasks):
     
 
 
-app = FastAPI()
-inventory={}
-
-
-@app.post("/process_link")
-async def process_link(link: str = Form(...)):
-    exec=getIt(link)
-    inventory[0]=exec
-    return inventory[0]
-
-@app.get("/", response_class=HTMLResponse)
-async def show_form(request: Request):
-    return """
-        <html>
-            <head>
-                <title>Get link and process</title>
-            </head>
-            <body>
-                <form method="post" action="/process_link">
-                    <input type="text" name="link">
-                    <button type="submit">Process link</button>
-                </form>
-            </body>
-        </html>
-    """
- 
